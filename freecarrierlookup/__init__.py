95c35ed877a1b33ec37a4eba37f3421fed07b43f
Daniel Lenski <dlenski@gmail.com>
2019-10-23 12:20:24 -0700

    tweak API calls to continue working
    - change `getcarrier.php` → `getcarrier_free.php`
    - need to update cookies with `HEAD /` before *every* lookup, not just first one

--- freecarrierlookup/__init__.py
+++ freecarrierlookup/__init__.py
@@ -26,27 +26,22 @@ class FreeCarrierLookup(object):
             else:
                 del s.headers['User-Agent']
             s.headers['Accept-Language'] = 'en'
-        self.connected = False
 
     def _connect(self):
-        # need cookies required for subsequent lookups to succeed
-        if not self.connected:
-            resp = self.session.head('https://freecarrierlookup.com')
-            self.connected = resp.ok
-            resp.raise_for_status()
+        # need cookies required for subsequent lookup to succeed
+        resp = self.session.get('https://freecarrierlookup.com')
+        resp.raise_for_status()
 
     def lookup(self, cc, phonenum):
         self._connect()
 
         # web interface includes test=456 and sessionlogin=0, but they don't seem to be required
-        resp = self.session.post('https://freecarrierlookup.com/getcarrier.php', {'cc':cc, 'phonenum':phonenum})
-        self.connected = resp.ok
+        resp = self.session.post('https://freecarrierlookup.com/getcarrier_free.php', {'cc':cc, 'phonenum':phonenum})
         resp.raise_for_status()
         try:
             j = resp.json()
             status, html = j['status'], j['html']
         except (ValueError, KeyError):
-            self.connected = False
             raise ValueError('Expected response to be JSON object containing status and html', resp.text)
 
         try:
